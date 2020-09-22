bc15b7f29555ad4f3d0beec47fd7b6b8856c87b2
Emilsone <lawsonezekiel68@gmail.com>
2020-09-22 12:29:43 +0100

    create dashboard

---
+++ src/components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+  <footer class="second-footer">
+    <div class="columns">
+      <div class="column has-text-white">
+        <ul class="underline">
+          <li class="footer-links">Legal</li>
+          <li class="footer-links">Privacy</li>
+          <li class="footer-links">Terms & condition</li>
+          <li class="footer-links">Cookies</li>
+          <li class="footer-links">Accessebility</li>
+          <li class="footer-links">Sitemap</li>
+        </ul>
+      </div>
+      <div class="column has-text-white">
+        <ul class="underline">
+          <li class="footer-links">Legal</li>
+          <li class="footer-links">Privacy</li>
+          <li class="footer-links">Terms & condition</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<style scoped>
+.footer {
+  background-color: #086858;
+}
+.input {
+  background-color: #085838;
+  border: 0px;
+}
+.line {
+  margin: 20px;
+  width: 95%;
+  background-color: darkgrey;
+}
+.columns {
+  padding-left: 20px;
+  padding-top: 60px;
+}
+li {
+  line-height: 45px;
+  font-size: 14px;
+}
+.links {
+  font-size: 25px;
+  font-weight: 100;
+}
+.second-footer {
+  background-color: #024731;
+  height: 29vh;
+}
+.underline {
+  float: left;
+}
+.footer-links {
+  display: inline-block;
+  padding: 1.2rem;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,134 @@
+<template>
+  <header>
+    <nav class="navbar is-fixed-top box-shadow-y">
+      <div class="navbar-brand">
+        <a
+          role="button"
+          class="navbar-burger toggler"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+
+        <a href="#" class="navbar-item has-text-weight-bold has-text-black">
+          Admin Dashboard
+        </a>
+        <a
+          role="button"
+          class="navbar-burger nav-toggler"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu has-background-white">
+        <div class="navbar-start">
+          <a href="#" class="navbar-item">
+            <i class="fas fa-home icon"></i> Home
+          </a>
+          <a href="#" class="navbar-item">
+            About
+          </a>
+          <a href="#" class="navbar-item">
+            Features
+          </a>
+          <a href="#" class="navbar-item">Pricing</a>
+        </div>
+        <div class="navbar-end">
+          <a href="#" class="navbar-item">
+            Notifications
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a href="#" class="navbar-link">
+              Admin
+            </a>
+            <div class="navbar-dropdown is-right">
+              <a href="#" class="navbar-item">
+                Profile
+              </a>
+              <a href="#" class="navbar-item">Settings</a>
+              <hr class="navbar-divider" />
+              <a href="#" class="navbar-item">Log Out</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="columns is-variable is-0">
+      <div class="menu-container px-1 has-background-white">
+        <div class="menu-wrapper py-1">
+          <aside class="menu">
+            <p class="menu-label has-text-lighter">General</p>
+            <ul class="menu-list">
+              <li>
+                <a href="#" class="has-text-black">
+                  <i class="fas fa-tachometer-alt icon"></i>
+                  Dashboard</a
+                >
+              </li>
+            </ul>
+            <p class="menu-label has-text-lighter">Administration</p>
+            <ul class="menu-list">
+              <li>
+                <a href="#" class="is-active has-background-primary">
+                  <i class="fas fa-cogs icon"></i>
+                  Settings</a
+                >
+              </li>
+              <li>
+                <a href="#" class="has-text-black">
+                  <i class="fas fa-users-cog icon"></i>
+                  Manage Team</a
+                >
+                <ul>
+                  <li>
+                    <a href="#" class="has-text-black">
+                      <i class="fas fa-users icon"></i>
+                      Members</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="has-text-black">
+                      <i class="fas fa-user-plus icon"></i>
+                      Add New</a
+                    >
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <p class="menu-label has-text-lighter">Other</p>
+            <ul class="menu-list">
+              <li>
+                <a href="#" class="has-text-black">
+                  <i class="fas fa-comments icon"></i>
+                  Chats</a
+                >
+              </li>
+              <li>
+                <a href="#" class="has-text-black">
+                  <i class="fas fa-info-circle icon"></i>
+                  Extras</a
+                >
+              </li>
+            </ul>
+          </aside>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+<style scoped>
+
+</style>
